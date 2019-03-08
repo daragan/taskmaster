9419532de180bca71718f15fc516e2c903d7aec4
Azraf Chowdhury <achowdhur@ltu.edu>
2019-03-08 09:26:13 +0000

    added post for tasks view

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,6 +1,6 @@
 class TaskController < ApplicationController
 
-  get '/tasks' do
+  get '/tasks/tasks' do
     redirect_if_not_logged_in
     @tasks = Task.all
     erb :'/tasks/tasks'
@@ -10,6 +10,27 @@ class TaskController < ApplicationController
     redirect_if_not_logged_in
     erb :'/tasks/new'
   end
+
+  post '/tasks/tasks' do
+    if logged_in?
+      if params["task"]["name"] == "" || params["task"]["content"] == ""
+        #flash[:message] = "Please fill in all required fields."
+        redirect to '/tasks/new'
+      else
+        @user = current_user
+        @task = Task.create(name: => params["task"]["name"], content: => params["task"]["content"], user_id: => session[:user_id])
+        if @task.save
+          @user.tasks << @task
+          redirect to '/tasks/tasks'
+        else
+          redirect to '/tasks/new'
+        end
+      end
+    else
+      redirect to '/users/login'
+    end
+  end
+
 
 
 end
