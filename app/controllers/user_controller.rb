3417286c125083955273ae90de00e8e07aa7a891
Azraf Chowdhury <achowdhur@ltu.edu>
2019-03-09 11:34:42 +0000

    refactored login and signup features

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,46 @@
 class UserController < ApplicationController
   enable :sessions
 
+#time to refactor RIP
+
+  get '/user/signup' do
+    if logged_in?
+      redirect '/tasks/tasks'
+    else
+      erb :'/user/signup'
+    end
+  end
+
+  post 'user/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect 'users/signup'
+    else
+      @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
+      session[:user_id] = @user.id
+      redirect '/tasks/tasks'
+    end
+  end
+
+  get '/user/login' do
+    if logged_in?
+      redirect '/tasks/tasks'
+    else
+      erb :'/user/login'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/tasks/tasks'
+    else
+      redirect 'user/login'
+    end
+  end
+
+
+
 
 
 #  get '/users/:id' do
